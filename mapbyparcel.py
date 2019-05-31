9a531a5fbd79ba646bbcf4c5df16acbef2bd8a6a
George Riner <37387327+nwicRiner@users.noreply.github.com>
2019-05-30 17:27:34 -0700

    switch generated output to file geodatabase
    since discovering that .SHP files cannot store or process NULL values in a field, and the result of doing so was populating our database with Trinomial values of '0' (zero) instead of <Null>.  Using a file geodatabase processes Null values correctly, and it seems slightly faster, too.

--- mapbyparcel.py
+++ mapbyparcel.py
@@ -21,6 +21,15 @@
 # 2018-03-06
 #	update to get icdb connection parameters from the system environment variables
 #		(see corresponding versions of PrepBatchTool.py)
+#
+# 2019-05-23
+#	need to test what the TrinNo value is in the tblResource record in ICDB
+#		only copy it over to the output shape field if it's non-zero
+#		because copying over whatever the dictionary reference returns is putting a lot of '0' values in TrinNo attribute in GIS
+#		and we need to stop doing that!
+#	based on: https://gis.stackexchange.com/questions/122780/how-to-allow-null-values-in-a-feature-layer
+#		the problem is that this code outputs a shape file. and shapefiles don't allow Null values (at least for INT fields)
+#		the correct solution is to write the output to a poly feature in a file geodatabase... Ugh.
 
 
 import openpyxl
@@ -34,11 +43,13 @@ import re
 
 DBsaved_selection_file =		arcpy.GetParameterAsText(0)									# input file path of saved selections
 
+map_by_APN_gdb = "mapByAPN.gdb"					# filename of the geodatabase to create/use for output
+
 run_user = getpass.getuser()					# get the current user's login id
 run_date = datetime.datetime(datetime.datetime.now().year,datetime.datetime.now().month,datetime.datetime.now().day)	# used for DigDate
 
 #-----
-# derive an output .shp file based on the saved selection file name....
+# derive an output .shp filename based on the saved selection file name....
 #	'x' is an input path/file to use as a stem for the output file location and filename
 #	'stem' is a short string to add to the filename as a way of identifying the output
 #	'ext' is the file type/extension to use
@@ -47,7 +58,7 @@ def make_output_file(x,stem,ext):
 	(x_base, x_ext) = os.path.splitext(x_file)	# split apart into <Drive:/path/filename> <extension>
 	remove_letters = u"~`!@#$%^&*()+-={}[]|\\:;<>?/.,\""		# list of characters to convert to '_'
 	trans = dict((ord(char),u'_') for char in remove_letters)	# f-ing UniCode #*%^@&!!
-	t = ""						# do our own translate loop
+	t = ""										# do our own translate loop
 	#arcpy.AddMessage("x_base = {0}".format(x_base))
 	for c in x_base:
 		if c in remove_letters:
@@ -68,6 +79,50 @@ def make_output_file(x,stem,ext):
 		if not os.path.exists(seq_try):
 			return seq_try
 		seq += 1						# bump up and loop around to try the next one
+
+#--------------------
+# create output feature
+#
+#	creates a file geodatabase in the directory
+#	creates a feature in the file geodatabase
+#	returns (result,featurename)
+#		if result=True, then featurename contains the pathname to the featureclass
+#		if result=False, then featurename contains an error msg string
+
+def create_output_feature(base_file,template_name):
+	(source_dir, source_file) = os.path.split(base_file)					# split apart the directory path from the filename
+	(source_basename, source_ext) = os.path.splitext(source_file)			# split the extension off the filename
+	map_by_apn_fgdb_path = os.path.join(source_dir,map_by_APN_gdb)			# path to fgdb
+	#arcpy.AddMessage(map_by_apn_fgdb_path)
+	if not arcpy.Exists(map_by_apn_fgdb_path):
+		arcpy.AddMessage("creating geodatabase for Map_by_APN outputs: {0}".format(map_by_apn_fgdb_path))
+		arcpy_result = arcpy.CreateFileGDB_management(source_dir,map_by_APN_gdb)
+		if arcpy_result.status != 4:										# 4 is the 'success' status result
+			return(False,"; ".join(arcpy_result.getMessages()))				# return fail and errors
+	# at this point the output geodatabase exists
+	# now, find the next available feature class name
+	remove_letters = u" ~`!@#$%^&*()+-={}[]|\\:;<>?/.,\""					# list of characters to convert to '_'
+	new_basename = ""														# do our own translate loop (f-cking Unicode)
+	for c in source_basename:
+		if c in remove_letters:
+			new_basename += "_"
+		else:
+			new_basename += c
+	#now, the basename is cleaned up, start searching for the next sequential feature
+	seq = 1
+	while (True):
+		try_feature = new_basename + "_APN_{0}".format(seq)
+		try_path = os.path.join(map_by_apn_fgdb_path,try_feature)
+		if not arcpy.Exists(try_path):
+			break
+		seq += 1
+	#arcpy.AddMessage("try to create {0}".format(try_feature))
+	arcpy_result = arcpy.CreateFeatureclass_management(map_by_apn_fgdb_path,try_feature,"POLYGON",template_name,"SAME_AS_TEMPLATE","SAME_AS_TEMPLATE",template_name)
+	if arcpy_result.status != 4:
+		return(False,"; ".join(arcpy_result.getMessages()))					# return fail and errors
+	return(True, arcpy_result.getOutput(0))									# it all worked, return True and the path to the feature class
+
+			
 
 #--------------------
 # start a connect to the ICDB
@@ -182,12 +237,19 @@ def map_reports():
 	arcpy.AddMessage("Mapping reports by APN")
 	# loop through each S-#
 	
-	# create the output shapefile
-	output_shapefile_name = make_output_file(DBsaved_selection_file,"parcelmapped","shp")
-	arcpy.AddMessage("output to: {0}".format(output_shapefile_name))
-	(out_path, out_name) = os.path.split(output_shapefile_name)
+	# create the output feature
 	out_template = r"MAIN\Reports\Reports (polygons)"
-	arcpy.CreateFeatureclass_management(out_path,out_name,"POLYGON",out_template,"SAME_AS_TEMPLATE","SAME_AS_TEMPLATE",out_template)
+	(success, output_shapefile_name) = create_output_feature(DBsaved_selection_file,out_template)
+	if not success:
+		arcpy.AddError(output_shapefile_name)
+		return
+
+	# create the output shapefile
+	#output_shapefile_name = make_output_file(DBsaved_selection_file,"parcelmapped","shp")
+	#arcpy.AddMessage("output to: {0}".format(output_shapefile_name))
+	#(out_path, out_name) = os.path.split(output_shapefile_name)
+	#out_template = r"MAIN\Reports\Reports (polygons)"
+	#arcpy.CreateFeatureclass_management(out_path,out_name,"POLYGON",out_template,"SAME_AS_TEMPLATE","SAME_AS_TEMPLATE",out_template)
 	shp_shape_field = arcpy.Describe(output_shapefile_name).ShapeFieldName
 	
 	count_voids = 0			# count S-#'s marked "Void"
@@ -321,13 +383,18 @@ def map_resources():
 	arcpy.AddMessage("Mapping resources by APN")
 	# loop through each Primary#
 	
-	# create the output shapefile
-	output_shapefile_name = make_output_file(DBsaved_selection_file,"parcelmapped","shp")
-	arcpy.AddMessage("output to: {0}".format(output_shapefile_name))
-	(out_path, out_name) = os.path.split(output_shapefile_name)
+	# create the output feature
 	out_template = r"MAIN\Resources\Resources (polygons)"
-	arcpy.CreateFeatureclass_management(out_path,out_name,"POLYGON",out_template,"SAME_AS_TEMPLATE","SAME_AS_TEMPLATE",out_template)
-	shp_shape_field = arcpy.Describe(output_shapefile_name).ShapeFieldName
+	(success, output_shapefile_name) = create_output_feature(DBsaved_selection_file,out_template)
+	if not success:
+		arcpy.AddError(output_shapefile_name)
+		return
+	
+	# output_shapefile_name = make_output_file(DBsaved_selection_file,"parcelmapped","shp")
+	# arcpy.AddMessage("output to: {0}".format(output_shapefile_name))
+	# (out_path, out_name) = os.path.split(output_shapefile_name)
+	# arcpy.CreateFeatureclass_management(out_path,out_name,"POLYGON",out_template,"SAME_AS_TEMPLATE","SAME_AS_TEMPLATE",out_template)
+	shp_shape_field = arcpy.Describe(output_shapefile_name).ShapeFieldName				# get the fieldname of the field that holds the geometry
 	
 	count_voids = 0			# count primary numbers marked "Void"
 	count_noAPN = 0			# count number of primarys with no APN in ICDB
@@ -412,7 +479,8 @@ def map_resources():
 				shp_new_row.setValue(shp_shape_field,apn_shape[1])
 				shp_new_row.setValue('PrimCo',p_co)
 				shp_new_row.setValue('PrimNo',p_no)
-				shp_new_row.setValue('TrinNo',icdb_resource_parent['TrinNo'])
+				if icdb_resource_parent['TrinNo'] > 0:					# if TrinNo is non-zero then copy it over, else leave Null
+					shp_new_row.setValue('TrinNo',icdb_resource_parent['TrinNo'])
 				shp_new_row.setValue('OtherID',icdb_resource_name)
 				shp_new_row.setValue('DocSource','p')					# selects 'parcel (APN)'
 				shp_new_row.setValue('DigSource','p')					# selects 'parcel (APN)'
@@ -497,7 +565,7 @@ elif sheet == sheet_saved_resources:
 	arcpy.AddMessage("{0} Primary #'s input".format(len(ResList)))
 	map_resources()
 
-# we're here because the saved selections are for neither reports nor resources (prolly wrong file picked)
+# we're here because the saved selections are for neither reports nor resources (prolly wrong file picked?)
 else:
-	arcpy.AddError("File is not a valid saved selections")
+	arcpy.AddError("File is not a valid ICDB saved selections")
 	sys.exit(1)
